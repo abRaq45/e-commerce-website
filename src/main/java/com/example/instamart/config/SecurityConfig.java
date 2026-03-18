8192ce611d8d7c817d629c64e41cdace05d8797c
abRaq45 <abdulraquib310@gmail.com>
2026-03-18 21:40:24 +0530

    Add allowed origin for CORS configuration

--- src/main/java/com/example/instamart/config/SecurityConfig.java
+++ src/main/java/com/example/instamart/config/SecurityConfig.java
@@ -42,7 +42,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+       configuration.setAllowedOrigins(List.of(
+    "http://localhost:5173",
+    "https://e-commerce-website-i4agmcmsh-abdul-raquibs-projects.vercel.app"
+));
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
